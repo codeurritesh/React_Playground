35b7d44ccd05fb6d205fae947280b291305b1938
codeurritesh <riteshponty@gmail.com>
2023-11-27 14:23:24 +0530

    react app

--- my-first-app/src/components/Todos.js
+++ my-first-app/src/components/Todos.js
@@ -1,6 +1,14 @@
+const Alltodos=["homeword","Shopping","Eating","Coding","Project","Schlorship"];
 const todos=()=>{
 return (
-    a
+    <div className="person">
+        <h3>All todos</h3>
+        <ul>
+            {Alltodos.map((item)=>{
+                return <h4><li>{item}</li></h4>;
+            })}
+        </ul>
+    </div>
 )
 
 }
