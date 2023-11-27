083a56ddce62542b7351ba4525c75232d5129392
codeurritesh <riteshponty@gmail.com>
2023-11-27 21:29:33 +0530

    final for today

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -7,11 +7,11 @@ const app=()=>{
     <div className="middlebox">
       <Mood/>
       <Todo/>
+      
       <Person name="Ritesh Verma" age="22" company="Big oh notations"/>
       <Person name="Tanya Bansal" age="21" company="TATA AIG"/>
       <Person name="Aryan UPa" age="24" company="221B Bakers Street"/>
       <Person name="Rishabh Agrawal" age="22" company="TATA AIG"/>
-      
 
     </div>
   
--- my-first-app/src/components/Todos.js
+++ my-first-app/src/components/Todos.js
@@ -1,6 +1,14 @@
 const Alltodos=["homeword","Shopping","Eating","Coding","Project","Schlorship"];
+// const addbutton=document.getElementById("addtodo");
+// // let clicked=false;
+// addbutton.addEventListener('click',()=>{
+// alert('clicked');})
+// console.log(clicked);
 const todos=()=>{
+   
+        
 return (
+    <div>
     <div className="person">
         <h3>All todos</h3>
         <ul>
@@ -8,6 +16,11 @@ return (
                 return <h4><li>{item}</li></h4>;
             })}
         </ul>
+        <button className="btn btn-dark" id="addtodo">Add new todo</button>
+
+    </div>
+    
+  
     </div>
 )
 
