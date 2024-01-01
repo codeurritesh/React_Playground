d88c857935ea56a3a39dbf15b8be0045ebfbe8e0
codeurritesh <riteshponty@gmail.com>
2024-01-01 15:59:51 +0530

    react router dom

--- react_router_demo/src/components/Pages/Dog.js
+++ react_router_demo/src/components/Pages/Dog.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Dog() {
+    const navigate=useNavigate();
   return (
     <div>
         <h1>This is Dog Page</h1>
+        <button onClick={()=>{navigate('/')}}>Click to go Main Page</button>
     </div>
   )
 }
